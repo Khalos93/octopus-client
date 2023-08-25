5d9405b934983ee11e539b1d4b625b7c7c78fd0f
Riccardo <r.golin@hotmail.it>
2023-08-25 10:37:28 +0100

    skelethon structure created for main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.scss';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="header-component"></div>
+      <div className="main-display"></div>
+      <div className="fancy-buttons"></div>
+      <div className="impossible-graph"></div>
+      <div className="nav-bar"></div>
+    </>
+  );
 }
 
 export default App;
