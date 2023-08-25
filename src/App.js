c7281027d6f1ba5d27de3fed26e9fbb6c8273bf5
louie176 <louiegay@aol.com>
2023-08-25 11:53:42 +0100

    main display generally complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.scss";
+import Header from "./Component/Header/Header";
+import MainDisplay from "./Component/MainDisplay/MainDisplay";
 import Nav from "./Component/Nav/Nav";
 
 function App() {
   return (
     <>
-      <div className="header-component"></div>
-      <div className="main-display"></div>
-      <div className="fancy-buttons"></div>
-      <div className="impossible-graph"></div>
-      <div className="nav-bar">
+      <div className="app__header-component">
+        <Header />
+      </div>
+      <div className="app__main-display">
+        <MainDisplay />
+      </div>
+      <div className="app__fancy-buttons"></div>
+      <div className="app__impossible-graph"></div>
+      <div className="app__nav-bar">
         <Nav />
       </div>
     </>
